05d59e4b165c66a7f5ae6f9ae23db5bed81d60ae
defalt <deniscotropitorul@gmail.com>
2025-01-12 11:37:09 +0200

    FINISHED

--- src/guiForms/profDash.java
+++ src/guiForms/profDash.java
@@ -2,7 +2,9 @@ package guiForms;
 
 import claseBaza.Curs;
 import claseBaza.Profesor;
+import claseBaza.Student;
 import claseFunctionalitate.FileDataManager;
+import claseFunctionalitate.FileDisplay;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -25,6 +27,7 @@ public class profDash  extends JFrame {
         setVisible(true);
         setResizable(false);
         FileDataManager fileDataManager = new FileDataManager();
+        FileDisplay fileDisplay = new FileDisplay();
         List<Curs> cursList = fileDataManager.createCoursesData();
         welcomeLabel.setText("Welcome " + profesor.getPrenume() + "!");
         logoutButton.addActionListener(new ActionListener() {
@@ -37,15 +40,97 @@ public class profDash  extends JFrame {
         afiseazaCursuriPredateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                StringBuilder deAfis=new StringBuilder();
+                deAfis.append("Cursurile Predate de dvs: \n");
                 for(Curs curs:cursList){
                     if(curs.getProfesor().getId()==profesor.getId()){
-
+                        deAfis.append(curs.getId()+" "+curs.getNume()+" "+curs.getDescriere());
                     }
 
                 }
+                JOptionPane.showMessageDialog(null,deAfis.toString(),"Cursuri",JOptionPane.PLAIN_MESSAGE);
+
+            }
+        });
+        afiseazaStudentiiUnuiCursButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StringBuilder deAfis=new StringBuilder();
+                deAfis.append("Cursurile Predate de dvs: \n");
+                for(Curs curs:cursList){
+                    if(curs.getProfesor().getId()==profesor.getId()){
+                        deAfis.append(curs.getId()+" "+curs.getNume()+" "+curs.getDescriere());
+                    }
+
+                }
+                String id =JOptionPane.showInputDialog(null,deAfis.toString(),"Alege id-ul cursului",JOptionPane.PLAIN_MESSAGE);
+                int idd=Integer.parseInt(id);
+                StringBuilder deAfiss=new StringBuilder();
+                for(Curs curs : cursList){
+                    if(curs.getId()==idd){
+                        for(Student student : curs.getStudenti()){
+                            deAfiss.append(student.getId()+" "+student.getNume()+" "+student.getPrenume()+" "+curs.getNote().get(student)+"\n");
+
+                        }
+
+                    }
+                }
+                JOptionPane.showMessageDialog(null,deAfiss.toString());
+
+
+
+            }
+        });
+        noteazaUnStudentButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StringBuilder deAfis=new StringBuilder();
+                deAfis.append("Cursurile Predate de dvs: \n");
+                for(Curs curs:cursList){
+                    if(curs.getProfesor().getId()==profesor.getId()){
+                        deAfis.append(curs.getId()+" "+curs.getNume()+" "+curs.getDescriere());
+                    }
+
+                }
+                String id =JOptionPane.showInputDialog(null,deAfis.toString(),"Alege id-ul cursului",JOptionPane.PLAIN_MESSAGE);
+                int idd=Integer.parseInt(id);
+                StringBuilder deAfiss=new StringBuilder();
+                for(Curs curs : cursList){
+                    if(curs.getId()==idd){
+                        for(Student student : curs.getStudenti()){
+                            deAfiss.append(student.getId()+" "+student.getNume()+" "+student.getPrenume()+" "+curs.getNote().get(student)+"\n");
+
+                        }
+
+                    }
+                }
+                String iddd=JOptionPane.showInputDialog(null,deAfiss.toString(),"Introdu ID-ul studentului",JOptionPane.PLAIN_MESSAGE);
+                int iddd1=Integer.parseInt(iddd);
+                String nota=JOptionPane.showInputDialog(null,"Introdu nota","Introdu nota",JOptionPane.PLAIN_MESSAGE);
+                int notaa=Integer.parseInt(nota);
+                for(Curs curs:cursList){
+                    if(String.valueOf(curs.getId()).equals(id)){
+                        for(Student s: curs.getStudenti()){
+                            if(String.valueOf(s.getId()).equals(iddd)){
+                                curs.getNote().put(s,notaa);
+                            }
+                        }
+                    }
+                }
+                Curs[] aux=new Curs[cursList.size()];
+                int contorrr=0;
+                for(Curs curs:cursList){
+                    aux[contorrr]=curs;
+                    contorrr++;
+                }
+                fileDisplay.displayNote(aux);
+
+
+
 
             }
         });
     }
+
 
 }
